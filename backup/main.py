1c5659060943b9682535d798d404728292ffd881
Anisimova Karina <51861302+Karina5005@users.noreply.github.com>
2022-11-06 22:13:52 +0300

    #3: Add argument parser (#8)

--- backup/main.py
+++ backup/main.py
@@ -1,2 +1,64 @@
+import argparse
+import os
+import logging
+import sys
+from typing import Optional
+
+
+class Parser(argparse.ArgumentParser):
+    def error(self, message):
+        raise argparse.ArgumentError(None, message)
+
+
+class Backup:
+    token = str
+    output_dir = str
+    organization = str
+    repositories = Optional[list]
+
+    def __init__(self, token, output_dir, organization, repositories):
+        self.token = token
+        self.organization = organization
+        self.output_dir = output_dir
+        self.repositories = repositories
+        if not os.path.isdir(self.output_dir):
+            logging.warning('Output directory does not exist. It will be created')
+            os.mkdir(self.output_dir)
+
+
+def parse_args(args=None) -> argparse.Namespace:
+    parser = Parser(description='Backup a GitHub organization')
+    parser.add_argument('organization',
+                        metavar='ORGANIZATION_NAME',
+                        type=str,
+                        help='github organization name')
+    parser.add_argument('-t',
+                        '--token',
+                        type=str,
+                        default='',
+                        dest='token',
+                        help='personal token')
+    parser.add_argument('-o',
+                        '--output-directory',
+                        type=str,
+                        default='.',
+                        dest='output_dir',
+                        help='directory for backup')
+    parser.add_argument('-r',
+                        '--repository',
+                        nargs='+',
+                        default='',
+                        dest='repository',
+                        help='name of repositories to limit backup')
+    parsed = parser.parse_args(args)
+    return parsed
+
+
 if __name__ == "__main__":
-    print("Hello world")
+    parsed_args = None
+    backup = None
+    try:
+        parsed_args = parse_args(sys.argv[1:])
+        backup = Backup(parsed_args.token, parsed_args.output_dir, parsed_args.organization, parsed_args.repository)
+    except argparse.ArgumentError as e:
+        logging.error(e.message)
---
+++ tests/backup_test.py
@@ -0,0 +1,36 @@
+import argparse
+import os
+
+from backup.main import parse_args, Backup
+import pytest
+
+
+class TestArgs:
+    def test_organization(self):
+        args_parsed = parse_args(['test_organization'])
+        assert args_parsed.organization == 'test_organization'
+
+    def test_empty_organization(self):
+        with pytest.raises(argparse.ArgumentError):
+            parse_args(['-t', 'token'])
+
+    def test_token(self):
+        args_parsed = parse_args(['-t', 'token', 'test_organization'])
+        assert args_parsed.organization == 'test_organization'
+        assert args_parsed.token == 'token'
+
+    def test_output_dir(self):
+        args_parsed = parse_args(['-t', 'token', '-o', 'test_dir', 'test_organization'])
+        assert args_parsed.output_dir == 'test_dir'
+
+    def test_default_output_dir(self):
+        args_parsed = parse_args(['-t', 'token', 'test_organization'])
+        assert args_parsed.output_dir == '.'
+
+
+class TestBackup:
+    def test_bad_dir(self):
+        assert not os.path.isdir('tmp')
+        Backup('token', 'tmp', 'organization', None)
+        assert os.path.isdir('tmp')
+        os.rmdir('tmp')
