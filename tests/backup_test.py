71587b243117b461920aacc5aaf122e8c5e3ea37
Karina Anisimova <anisimova5005@gmail.com>
2022-11-28 13:51:26 +0300

    fix tests of issues

--- tests/backup_test.py
+++ tests/backup_test.py
@@ -48,6 +48,11 @@ class TestBackup:
         }
     ]
 
+    def check_json(self, expected, path):
+        assert os.path.isfile(path)
+        actual = json.load(open(path))
+        assert expected == actual
+
     def test_backup_members(self):
         with requests_mock.Mocker() as m:
             m.get(url='https://api.github.com/orgs/org/members',
@@ -75,23 +80,54 @@ class TestBackup:
                   json.load(open(f'{self.backup.output_dir}/members/test2/member.json'))]
         assert actual == self.users
 
-        assert os.path.isfile(f'{self.backup.output_dir}/members/test1/membership.json')
-        assert os.path.isfile(f'{self.backup.output_dir}/members/test2/membership.json')
-        test1_expected = {
-            "role": "admin",
-            "state": "active"
-        }
-        test2_expected = {
-            "role": "member",
-            "state": "active"
-        }
-        test1 = json.load(open(f'{self.backup.output_dir}/members/test1/membership.json'))
-        test2 = json.load(open(f'{self.backup.output_dir}/members/test2/membership.json'))
-        assert test1 == test1_expected
-        assert test2 == test2_expected
+        self.check_json({"role": "admin", "state": "active"}, f'{self.backup.output_dir}/members/test1/membership.json')
+        self.check_json({"role": "member", "state": "active"},
+                        f'{self.backup.output_dir}/members/test2/membership.json')
 
     def test_backup_issues(self):
-        os.makedirs(self.backup.output_dir + "/repositories/test")
+        os.makedirs(self.backup.output_dir + "/repos/test")
+        with requests_mock.Mocker() as m:
+            m.get(url='https://api.github.com/repos/org/test/issues',
+                  request_headers={'Accept': 'application/vnd.github+json',
+                                   'Authorization': 'Bearer token'},
+                  response_list=[{'json': [
+                      {
+                          'number': 1,
+                          'title': 'test',
+                          'body': 'test description',
+                          'created_at': '2022-10-18T13:00:57Z',
+                          'user': {'login': 'login'},
+                          'state': 'closed',
+                          'assignee': {'login': 'assignee'},
+                          'html_url': 'https://github.com/normal-issue'
+                      },
+                      {
+                          'number': 2,
+                          'title': 'test2',
+                          'body': 'test description',
+                          'created_at': '2022-10-11T13:00:57Z',
+                          'user': {'login': 'login'},
+                          'state': 'open',
+                          'assignee': {'login': 'assignee'},
+                          'html_url': 'https://github.com/pull'
+                      }
+                  ], 'status_code': 200}])
+            m.get(url='https://api.github.com/repos/org/test/issues/1/comments',
+                  request_headers={'Accept': 'application/vnd.github+json',
+                                   'Authorization': 'Bearer token'},
+                  response_list=[{'json': [], 'status_code': 200}])
+            self.backup.backup_issues()
+        assert not os.path.exists(self.backup.output_dir + '/repos/test/issues/2')
+        assert os.path.exists(self.backup.output_dir + '/repos/test/issues/1/comments')
+
+        self.check_json(
+            {"title": "test", "body": "test description", "created_at": "2022-10-18T13:00:57Z", "state": "closed"},
+            f'{self.backup.output_dir}/repos/test/issues/1/issue.json')
+        self.check_json({'login': 'login'}, f'{self.backup.output_dir}/repos/test/issues/1/user.json')
+        self.check_json({'login': 'assignee'}, f'{self.backup.output_dir}/repos/test/issues/1/assignee.json')
+
+    def test_backup_issues_comments(self):
+        os.makedirs(self.backup.output_dir + "/repos/test", exist_ok=True)
         with requests_mock.Mocker() as m:
             m.get(url='https://api.github.com/repos/org/test/issues',
                   request_headers={'Accept': 'application/vnd.github+json',
@@ -123,41 +159,25 @@ class TestBackup:
                                    'Authorization': 'Bearer token'},
                   response_list=[{'json': [
                       {
+                          'id': 1,
                           'body': 'comment1',
                           'created_at': '2022-10-24T10:05:33Z',
                           'user': {'login': 'login1'},
                       },
                       {
+                          'id': 2,
                           'body': 'comment2',
                           'created_at': '2022-10-25T10:05:33Z',
                           'user': {'login': 'login2'},
                       }
                   ], 'status_code': 200}])
             self.backup.backup_issues()
-        expected = {
-            "title": "test",
-            "description": "test description",
-            "creation_date": "2022-10-18T13:00:57Z",
-            "creator_login": "login",
-            "status": "closed",
-            "assignee_login": "assignee",
-            "comments": [
-                {
-                    "comment": "comment1",
-                    "creation_date": "2022-10-24T10:05:33Z",
-                    "creator_login": "login1"
-                },
-                {
-                    "comment": "comment2",
-                    "creation_date": "2022-10-25T10:05:33Z",
-                    "creator_login": "login2"
-                }
-            ]
-        }
-        assert os.path.isfile(self.backup.output_dir + "/repositories/test/issues/" + "1.json")
-        assert not os.path.isfile(self.backup.output_dir + "/repositories/test/issues/" + "2.json")
-        actual = json.load(open(self.backup.output_dir + "/repositories/test/issues/" + "1.json"))
-        assert actual == expected
+        self.check_json({'id': 1, 'body': 'comment1', 'created_at': '2022-10-24T10:05:33Z'},
+                        f'{self.backup.output_dir}/repos/test/issues/1/comments/1/comment.json')
+        self.check_json({'login': 'login1'}, f'{self.backup.output_dir}/repos/test/issues/1/comments/1/user.json')
+        self.check_json({'id': 2, 'body': 'comment2', 'created_at': '2022-10-25T10:05:33Z'},
+                        f'{self.backup.output_dir}/repos/test/issues/1/comments/2/comment.json')
+        self.check_json({'login': 'login2'}, f'{self.backup.output_dir}/repos/test/issues/1/comments/2/user.json')
 
     def test_backup_pull(self):
         os.makedirs(self.backup.output_dir + "/repositories/test", exist_ok=True)
